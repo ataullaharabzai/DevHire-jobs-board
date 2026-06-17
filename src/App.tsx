bd89e3abc9b5dad81f1a2a8857318b0aaa2a8436
Ataullah Arabzai <ataullahforpc@gmail.com>
2026-06-17 10:39:47 +0430

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <i className="fa-solid fa-magnifying-glass"></i>
+
+      <i className="fa-solid fa-location-dot"></i>
+
+      <i className="fa-solid fa-bookmark"></i>
+    </>
+  )
+}
+
+export default App
